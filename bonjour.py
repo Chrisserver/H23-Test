57b9b458646576a53816fdfe8559a30a7b9b8fc3
Chrisserver <66754560+Chrisserver@users.noreply.github.com>
2023-01-25 13:51:27 -0500

    Create bonjour.py
    Chanson

---
+++ bonjour.py
@@ -0,0 +1,1 @@
+print("Le coeur est dans l'oiseau, l'oiseau est l'oeuf, l'oeuf est dans le nid, le nid est dans le trou, le trou est dans le noeud, le noeud est dans la branche, la branche est dans l'arbre!")
